b39bd2cdf2bf7d1df48a7a16ace9c1c09fc944f7
sgraziola <simo.fdl@gmail.com>
2022-11-08 14:34:22 +0100

    EmailList: add refacturing

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,17 +8,24 @@ createApp({
             maxEmails : 10,
         }
     },
+    methods: {
+        getEmails(){
+            const url = "https://flynn.boolean.careers/exercises/api/random/mail"
+            for (let i = 0; i < this.maxEmails; i++) {
+                
+                axios
+                .get(url)
+                .then(response => {
+                    this.randomEmails.push(response.data.response)
+                })
+                console.log(this.randomEmails);
+            }
+
+        }
+    },
 
     mounted(){
-        for (let i = 0; i < 10; i++) {
-            
-            axios
-            .get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then(response => {
-                this.randomEmails.push(response.data.response)
-            })
-            console.log(this.randomEmails);
-        }
+        this.getEmails()
     }
 
 }).mount("#app")
